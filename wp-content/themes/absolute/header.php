cfc0493bcc3036eb8d37f3c3884d5f0fec7cb8bd
Sergey Bulgacov <bulgakows@gmail.com>
2013-07-29 23:29:34 +0300

    brake

--- wp-content/themes/absolute/header.php
+++ wp-content/themes/absolute/header.php
@@ -26,7 +26,6 @@
     </title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <?php wp_head(); ?>
 </head>
